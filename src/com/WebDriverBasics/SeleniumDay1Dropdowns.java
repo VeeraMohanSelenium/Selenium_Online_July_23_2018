23a9177322490ab564af1591ccb3292e1f589c85
USER <USER@USER-PC>
2018-08-08 17:38:09 +0530

    commit1.11

--- src/com/WebDriverBasics/SeleniumDay1Dropdowns.java
+++ src/com/WebDriverBasics/SeleniumDay1Dropdowns.java
@@ -1,5 +1,7 @@
 package com.WebDriverBasics;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,15 +21,30 @@ public class SeleniumDay1Dropdowns {
 		Select sc=new Select(dayField);
 		Thread.sleep(3000);
 		sc.selectByIndex(15);
+		
+		String selected_value=sc.getFirstSelectedOption().getText();
+		System.out.println(selected_value);
 		Thread.sleep(3000);
 		sc.selectByValue("9");
+		
+		selected_value=sc.getFirstSelectedOption().getText();
+		System.out.println(selected_value);
 		Thread.sleep(3000);
 		sc.selectByVisibleText("Day");
+		//getFirstSelectedOptions method is used to get the select value iin Dropdowns
+	
+		selected_value=sc.getFirstSelectedOption().getText();
+		
+		//getFirstSelectedOption method is used to get the  all select value in Dropdowns
+		List<WebElement> list=sc.getAllSelectedOptions();
+		System.out.println(selected_value);
 		
 		//driver.findElement(By.name("birthday_month"));
 		sc=new Select(driver.findElement(By.name("birthday_month")));
 		
 		sc.selectByVisibleText("May");
+		selected_value=sc.getFirstSelectedOption().getText();
+		System.out.println(selected_value);
 
 	}
 
---
+++ src/com/WebDriverBasics/SeleniumLocaters.java
@@ -0,0 +1,48 @@
+package com.WebDriverBasics;
+
+public class SeleniumLocaters {
+
+	public static void main(String[] args) {
+		//Locaters
+		//Locaters are used to identified by the Exeact Webelement in the WebApplications
+		
+		//Types of Locaters
+		// 1. Id
+		//driver.FindElement(By.id(""));
+		
+		// 2. name
+		//driver.FindElement(By.name(""));
+		
+		// 3. className
+		//driver.FindElement(By.className(""));
+		
+		// 4. TagName
+		// driver.FindElement(By.tagName(""));
+		
+		// 5. linktext
+		// driver.FindElement(By.linkText("OrangeHRM, Inc."));
+		
+		// 6. Partiallinktext
+		// driver.findElement(by.partiallinkText("OrangeHRM,"));
+		
+		// 7. X-path
+		// Very fasltly to identified the element
+		// Types  of X-path
+		// 1. Abslouate X-path
+		// We have identified the element starting node to Taeget node of Tag names
+		// Is starts with single forword slash "/"
+		
+		//Ex: html/body/div[1]/div[3]/div[1]/div/div/div/div[1]/div[1]/a
+		
+		
+		// 2. Partial or Relative x-Path
+		
+		// We have identified  the element direct Webelement
+		
+		
+		
+		// 8. Css Selector
+
+	}
+
+}
